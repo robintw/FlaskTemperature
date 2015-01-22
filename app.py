09dff463552d6c7c3d7e94e7eeb2957198feb89e
Robin Wilson <robin@rtwilson.com>
2015-01-22 16:08:06 +0000

    First Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from get_temp import get_temperature
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/sensor/temperature")
+def hello():
+    return get_temperature()
+
+if __name__ == "__main__":
+    app.run()
