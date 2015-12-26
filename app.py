dcb3de30777f5177a1c6b52502324e7dd1bc134b
Robin Wilson <robin@rtwilson.com>
2015-12-26 22:19:20 +0000

    Tidying & PEP8

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from flask import Flask
+
 from get_temp import get_temperature
 
-from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/temperature")
-def hello():
+def temperature():
     return str(get_temperature())
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- get_temp.py
+++ get_temp.py
@@ -1,10 +1,10 @@
 def get_temperature():
-	with open("/sys/bus/w1/devices/28-000005fc565e/w1_slave") as f:
-		s = f.read()
-		s = s.split('\n')[1].split(' ')[9]
-		temperature = float(s[2:]) / 1000.0
+    with open("/sys/bus/w1/devices/28-000005fc565e/w1_slave") as f:
+        s = f.read()
+        s = s.split('\n')[1].split(' ')[9]
+        temperature = float(s[2:]) / 1000.0
 
-	return temperature
+    return temperature
 
 if __name__ == "__main__":
-	print get_temperature()
+    print(get_temperature())
